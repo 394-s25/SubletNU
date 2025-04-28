d89498162132f93e3d40325557147fe0cd102b7b
zoryah-gray <zoryahgray2025@u.northwestern.edu>
2025-04-28 09:41:09 -0500

    added basic top banner alert

--- SubletNU/src/pages/HomePage.jsx
+++ SubletNU/src/pages/HomePage.jsx
@@ -4,9 +4,10 @@ import Listing from "../components/Listing";
 import PageWrapper from "../components/PageWrapper";
 import CreateListingModal from "../components/CreateListingModel";
 import AlertModal from "../components/AlertModal";
+import TopAlert from "../components/TopAlert";
 import LeafletMapBox from "../components/LeafletMapBox";
-import { db } from "../firebase";
-import { ref, get } from "firebase/database"; 
+import { db, auth } from "../firebase";
+import { ref, onValue } from "firebase/database"; 
 import "../css/home.css";
 
 export default function HomePage() {
@@ -19,7 +20,17 @@ export default function HomePage() {
   const [isAlertOpen, setAlertModal] = useState(false);
   const [alertModalMessage, setAlertModalMessage] = useState("");
   const [selectedMarker, setSelectedMarker] = useState({}); 
+  const [showTopAlert, setTopAlert] = useState(false);
   // ^to be used for filtering so user can see the listing they've just selected
+  const dbMatchReqRef = ref(db, "users/" + auth.currentUser.uid + "/userMatchRequests");
+  const dbMatchesRef = ref(db, "users/" + auth.currentUser.uid + "/userMatches");
+
+  useEffect(() => {
+    return onValue(dbMatchesRef, (snapshot) => {
+      if (snapshot.exists()) setTopAlert(true);
+    });
+
+  }, [dbMatchesRef]);
 
 
 
@@ -29,80 +40,94 @@ export default function HomePage() {
   };
 
   return (
-    <PageWrapper
-      onShowAll={() => {
-        setShowAllListings(true);
-        setShowUserListings(false);
-      }}
-      onShowUser={() => {
-        setShowAllListings(false);
-        setShowUserListings(true);
-      }}
-      onCreateNew={() => setIsCreateOpen(true)}
-    >
-      <div className="home-layout">
-        <div className="home-left">
-          <h2 className="home-title">Sublet Listings</h2>
-          <input
-            type="text"
-            placeholder="Search listing by keywords"
-            value={filter}
-            onChange={(e) => setFilter(e.target.value)}
-            className="home-input"
-          />
+    <div>
+      
+      {showTopAlert && (
+        <TopAlert
+          message="A new request or match has been made. Check your Profile for more information."
+          type="info" // or "error"
+          onClose={() => setTopAlert(false)}
+        />
+      )}
 
-          {showAllListings && (
-
-            <Listing setListings={setListings} 
-                      filter={filter} 
-                      setAlertModal={setAlertModal} 
-                      setAlertModalMessage={setAlertModalMessage}
-                      selectedMarker={selectedMarker}/>
-
-          )}
-
-          {showUserListings && (
-            <Listing setListings={setListings} 
-                      showOnlyCurrentUser={true} 
-                      setAlertModal={setAlertModal} 
-                      setAlertModalMessage={setAlertModalMessage}
-                      selectedMarker={selectedMarker} />
-          )}
+      <PageWrapper
+        onShowAll={() => {
+          setShowAllListings(true);
+          setShowUserListings(false);
+        }}
+        onShowUser={() => {
+          setShowAllListings(false);
+          setShowUserListings(true);
+        }}
+        onCreateNew={() => setIsCreateOpen(true)}
+      >
         
-        </div>
 
+        <div className="home-layout">
+          <div className="home-left">
+            <h2 className="home-title">Sublet Listings</h2>
+            <input
+              type="text"
+              placeholder="Search listing by keywords"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+              className="home-input"
+            />
 
-        <div className="home-map-container">
+            {showAllListings && (
 
+              <Listing setListings={setListings} 
+                        filter={filter} 
+                        setAlertModal={setAlertModal} 
+                        setAlertModalMessage={setAlertModalMessage}
+                        selectedMarker={selectedMarker}/>
 
+            )}
 
-
-
-          <LeafletMapBox
-            setSelectedMarker={setSelectedMarker}
-            listings={listings.filter((l) => l.lat && l.lng)
-              .map((l) => ({
-                lat: parseFloat(l.lat),
-                lng: parseFloat(l.lng),
-                ...l,
-              }))}
-          />
+            {showUserListings && (
+              <Listing setListings={setListings} 
+                        showOnlyCurrentUser={true} 
+                        setAlertModal={setAlertModal} 
+                        setAlertModalMessage={setAlertModalMessage}
+                        selectedMarker={selectedMarker} />
+            )}
           
+          </div>
+
+
+          <div className="home-map-container">
+
+
+
+
+
+            <LeafletMapBox
+              setSelectedMarker={setSelectedMarker}
+              listings={listings.filter((l) => l.lat && l.lng)
+                .map((l) => ({
+                  lat: parseFloat(l.lat),
+                  lng: parseFloat(l.lng),
+                  ...l,
+                }))}
+            />
+            
+          </div>
         </div>
-      </div>
 
-      <CreateListingModal
-        isOpen={isCreateOpen}
-        onClose={() => setIsCreateOpen(false)}
-        setAlertModal={setAlertModal}
-        setAlertModalMessage={setAlertModalMessage}
-      />
+        <CreateListingModal
+          isOpen={isCreateOpen}
+          onClose={() => setIsCreateOpen(false)}
+          setAlertModal={setAlertModal}
+          setAlertModalMessage={setAlertModalMessage}
+        />
 
-      <AlertModal 
-        isOpen={isAlertOpen} 
-        onClose={() => onAlertClose()} 
-        message={alertModalMessage}
-      />
-    </PageWrapper>
+        <AlertModal 
+          isOpen={isAlertOpen} 
+          onClose={() => onAlertClose()} 
+          message={alertModalMessage}
+        />
+
+      </PageWrapper>
+    </div>
   );
 }
